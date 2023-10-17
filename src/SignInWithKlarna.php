6ae02f16e2075c7521c316dd4c22f5655d89c63a
Montazar Al-Jaber <github.v0its@slmail.me>
2023-10-17 12:27:35 +0200

    Add plugin settings

--- src/SignInWithKlarna.php
+++ src/SignInWithKlarna.php
@@ -37,6 +37,46 @@ if ( ! class_exists( 'SignInWithKlarna' ) ) {
 		public static $siwk_placement_hook = 'siwk_placement';
 
 		/**
+		 * The UUID you received after the Sign in with Klarna onboarding.
+		 *
+		 * @var string
+		 */
+		private $client_id;
+		/**
+		 * The market or the country where this integration is available.
+		 *
+		 * @var string
+		 */
+		private $market;
+		/**
+		 * The environment to which the integration is pointing: playground or production.
+		 *
+		 * @var string 'playground' or 'production'.
+		 */
+		private $mode;
+
+		/**
+		 * The button's color theme.
+		 *
+		 * @var string
+		 */
+		private $button_theme;
+
+		/**
+		 * The button's shape.
+		 *
+		 * @var string
+		 */
+		private $button_shape;
+
+		/**
+		 * Change alignment of the Klarna logo on the call to action button based on the provided configuration.
+		 *
+		 * @var string
+		 */
+		private $logo_alignment;
+
+		/**
 		 * The reference the *Singleton* instance of this class.
 		 *
 		 * @var $instance
@@ -73,11 +113,122 @@ if ( ! class_exists( 'SignInWithKlarna' ) ) {
 		/**
 		 * Initialize so that SIWK can be displayed.
 		 *
-		 * Without this initialization, even if you output the button HTML to the document, it won't be displayed to the user.
+		 * @param array $args Plugin settings that should include the SIWK settings.
+		 * @return void
+		 */
+		public function init( $args = array() ) {
+			$args = wp_parse_args(
+				$args,
+				array(
+					'siwk_client_id'      => '',
+					'siwk_market'         => '',
+					'siwk_mode'           => 'playground',
+					'siwk_button_theme'   => 'default',
+					'siwk_button_shape'   => 'default',
+					'siwk_logo_alignment' => 'left',
+				)
+			);
+
+			$this->client_id      = $args['siwk_client_id'];
+			$this->market         = $args['siwk_market'];
+			$this->mode           = $args['siwk_mode'];
+			$this->button_theme   = $args['siwk_button_theme'];
+			$this->button_shape   = $args['siwk_button_shape'];
+			$this->logo_alignment = $args['siwk_logo_alignment'];
+
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+			include_once '/var/www/html/wp-content/plugins/sign-in-with-klarna/src/class-siwk-ajax.php';
+		}
+
+		/**
+		 * Extend your plugin with the required SIWK settings.
+		 *
+		 * @param array $settings Your plugin settings as an array.
+		 * @return array
+		 */
+		public static function add_siwk_settings( $settings ) {
+			$settings['siwk_title'] = array(
+				'title' => __( 'Sign in with Klarna', 'siwk' ),
+				'type'  => 'title',
+			);
+
+			$settings['siwk_client_id'] = array(
+				'name'        => 'siwk_client_id',
+				'title'       => __( 'Client ID', 'siwk' ),
+				'description' => __( 'The UUID you received after the Sign in with Klarna onboarding', 'siwk' ),
+				'type'        => 'text',
+			);
+
+			$settings['siwk_environment'] = array(
+				'name'        => 'siwk_environment',
+				'title'       => __( 'Environment', 'siwk' ),
+				'type'        => 'select',
+				'description' => __( 'The environment to which the integration is pointing.', 'siwk' ),
+				'default'     => 'playground',
+				'options'     => array(
+					'playground' => __( 'Playground', 'siwk' ),
+					'production' => __( 'Production', 'siwk' ),
+				),
+			);
+
+			$settings['siwk_market'] = array(
+				'name'        => 'siwk_market',
+				'title'       => __( 'Market', 'siwk' ),
+				'type'        => 'text',
+				'description' => __( 'The market or the country where this integration is available.', 'siwk' ),
+				'default'     => wc_get_base_location()['country'] ?? '',
+			);
+
+			$settings['siwk_button_theme'] = array(
+				'name'        => 'siwk_button_theme',
+				'title'       => __( 'Button theme' ),
+				'type'        => 'select',
+				'description' => __( 'The button\'s color theme.', 'siwk' ),
+				'default'     => 'default',
+				'options'     => array(
+					'default' => __( 'Default', 'siwk' ),
+					'dark'    => __( 'Dark', 'siwk' ),
+					'light'   => __( 'Light', 'siwk' ),
+				),
+			);
+
+			$settings['siwk_button_shape'] = array(
+				'name'        => 'siwk_button_shape',
+				'title'       => __( 'Button shape' ),
+				'type'        => 'select',
+				'description' => __( 'The button\'s shape.', 'siwk' ),
+				'default'     => 'default',
+				'options'     => array(
+					'default'   => __( 'Default', 'siwk' ),
+					'rectangle' => __( 'Rectangle', 'siwk' ),
+					'pill'      => __( 'Pill', 'siwk' ),
+				),
+			);
+
+			$settings['siwk_logo_alignment'] = array(
+				'name'        => 'siwk_logo_alignment',
+				'title'       => __( 'Logo alignment' ),
+				'type'        => 'select',
+				'description' => __( 'Change alignment of the Klarna logo on the call to action button based on the provided configuration.', 'siwk' ),
+				'default'     => 'left',
+				'options'     => array(
+					'left'   => __( 'Left', 'siwk' ),
+					'center' => __( 'Center', 'siwk' ),
+					'right'  => __( 'Right', 'siwk' ),
+				),
+			);
+
+			return $settings;
+		}
+
+		/**
+		 * Enqueue scripts.
+		 *
+		 * Determines whether the SIWK button should be rendered.
 		 *
 		 * @return void
 		 */
-		public function init() {
+		public function enqueue_scripts() {
 			/*
 			 * Check if we need to display the SIWK button:
 			 * 1. if logged in or guest but has not signed in with klarna.
@@ -87,18 +238,6 @@ if ( ! class_exists( 'SignInWithKlarna' ) ) {
 				return;
 			}
 
-			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-			add_action( 'script_loader_tag', array( $this, 'siwk_script_tag' ), 10, 2 );
-
-			include_once '/var/www/html/wp-content/plugins/sign-in-with-klarna/src/class-siwk-ajax.php';
-		}
-
-		/**
-		 * Enqueue scripts.
-		 *
-		 * @return void
-		 */
-		public function enqueue_scripts() {
 			$script_path = get_home_url( null, '/wp-content/plugins/klarna-payments-for-woocommerce/vendor/krokedil/sign-in-with-klarna/src/assets/siwk.js' );
 
 			// 'siwk_script' MUST BE registered before Klarna's lib.js
@@ -113,6 +252,9 @@ if ( ! class_exists( 'SignInWithKlarna' ) ) {
 
 			wp_register_script( self::$library_handle, 'https://x.klarnacdn.net/sign-in-with-klarna/v1/lib.js', array( 'siwk_script' ), '0.0.1', true );
 			wp_enqueue_script( self::$library_handle );
+
+			// Add data- attributes to the script tag.
+			add_action( 'script_loader_tag', array( $this, 'siwk_script_tag' ), 10, 2 );
 		}
 
 		/**
@@ -251,7 +393,7 @@ if ( ! class_exists( 'SignInWithKlarna' ) ) {
 			// Set Klarna as the selected payment method.
 			$gateways = WC()->payment_gateways->get_available_payment_gateways();
 			foreach ( $gateways as $gateway ) {
-				if ( in_array( $gateway->id, array( 'kco', 'klarna_payments' ) ) ) {
+				if ( in_array( $gateway->id, array( 'kco', 'klarna_payments' ), true ) ) {
 
 					// Set the highest ordered Klarna payment gateway.
 					WC()->session->set( 'chosen_payment_method', $gateway->id );
@@ -275,11 +417,11 @@ if ( ! class_exists( 'SignInWithKlarna' ) ) {
 				return $tag;
 			}
 
-			$locale      = apply_filters( 'siwk_locale', get_locale() );
-			$market      = 'SE';
-			$environment = 'playground';
-			$client_id   = 'd0c2a58e-7d61-4ffb-a444-3411fb344d40';
-			$scope       = 'offline_access profile phone email billing_address';
+			$locale      = esc_attr( apply_filters( 'siwk_locale', get_locale() ) );
+			$client_id   = esc_attr( apply_filters( 'siwk_client_id', $this->client_id ) );
+			$market      = esc_attr( apply_filters( 'siwk_market', $this->market ) );
+			$environment = esc_attr( apply_filters( 'siwk_environment', 'playground' === $this->mode ? 'playground' : 'production' ) );
+			$scope       = esc_attr( apply_filters( 'siwk_scope', 'offline_access profile phone email billing_address' ) );
 
 			return str_replace( ' src', " data-locale='{$locale}' data-market='{$market}' data-environment='{$environment}' data-client-id='{$client_id}' data-scope='{$scope}' data-on-sign-in='onSignIn' data-on-error='onSignInError' src", $tag );
 		}
@@ -290,11 +432,11 @@ if ( ! class_exists( 'SignInWithKlarna' ) ) {
 		 * @return void
 		 */
 		public function siwk_placement() {
-			$theme     = 'default'; // default, dark, light.
-			$shape     = 'default'; // default, rectangle, pill.
-			$alignment = 'left';
+			$theme     = esc_attr( apply_filters( 'siwk_button_theme', $this->button_theme ) ); // default, dark, light.
+			$shape     = esc_attr( apply_filters( 'siwk_button_shape', $this->button_shape ) ); // default, rectangle, pill.
+			$alignment = esc_attr( apply_filters( 'siwk_logo_alignment', $this->logo_alignment ) ); // left, right, center.
 
-			// phpcs:ignore -- must be output as HTML.
+			// phpcs:ignore -- must be echoed as html; attributes escaped.
 			echo "<klarna-sign-in data-theme='{$theme}' data-shape='{$shape}' data-logo-alignment='{$alignment}'></klarna-sign-in>";
 		}
 	}
