0439205c0b8fb863d1112b252196c2b0254dedc4
Montazar <26507935+mntzrr@users.noreply.github.com>
2023-11-30 02:34:14 +0100

    Add JWT validation and remove use of static

--- src/JWT.php
+++ src/JWT.php
@@ -1,5 +1,8 @@
-<?php
+<?php //phpcs:ignore -- PCR-4 compliant.
 namespace Krokedil\SignInWithKlarna;
+
+use Firebase\JWT\JWT as FirebaseJWT;
+use Firebase\JWT\JWK as FirebaseJWK;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -10,15 +13,93 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class JWT {
 
+
+	/**
+	 * The Klarna request's base URL.
+	 *
+	 * @var string
+	 */
+	private $base_url;
+
+	/**
+	 * Klarna JWKS URL.
+	 *
+	 * @var string
+	 */
+	private $jwks_url;
+
+	/**
+	 * JWKS
+	 *
+	 * @var array
+	 */
+	private $jwks;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param bool $test_mode Whether to use the test or production endpoint.
+	 */
+	public function __construct( $test_mode ) {
+		$this->base_url = 'https://' . ( $test_mode ? 'login.playground.klarna.com' : 'login.klarna.com' );
+		$this->jwks_url = $this->base_url . '/eu/lp/idp/.well-known/jwks.json';
+	}
+
+	/**
+	 * The Klarna request's base URL.
+	 *
+	 * @return string
+	 */
+	public function get_base_url() {
+		return $this->base_url;
+	}
+
+	/**
+	 * Check if a given JWT is valid.
+	 *
+	 * This includes signature verification.
+	 *
+	 * @param string $jwt_token The JWT token.
+	 * @return array|bool The JWT decoded if is valid, otherwise, FALSE.
+	 */
+	public function is_valid_jwt( $jwt_token ) {
+		if ( empty( $this->jwks ) ) {
+			$response = wp_remote_get(
+				$this->jwks_url,
+				array(
+					'headers' => array(
+						'Accept' => 'application/json',
+					),
+				)
+			);
+
+			if ( is_wp_error( $response ) ) {
+				return false;
+			}
+
+			$this->jwks = json_decode( wp_remote_retrieve_body( $response ), true );
+		}
+
+		try {
+			// Convert the stdClass to associative array.
+			return json_decode( wp_json_encode( FirebaseJWT::decode( $jwt_token, FirebaseJWK::parseKeySet( $this->jwks ) ) ), true );
+		} catch ( \Exception $e ) {
+
+			// Set to false to ensure new keys are retrieved next time this function is called.
+			$this->jwks = false;
+			return false;
+		}
+	}
+
 	/**
 	 * Extract the JWT payload.
 	 *
 	 * @param string $jwt_token The JWT token.
-	 * @return array The payload.
+	 * @return array|\WP_Error The payload.
 	 */
-	public static function get_jwt_payload( $jwt_token ) {
-		$segments = explode( '.', $jwt_token );
-		return json_decode( base64_decode( $segments[1] ), true );
+	public function get_payload( $jwt_token ) {
+		$payload = $this->is_valid_jwt( $jwt_token );
+		return empty( $payload ) ? new \WP_Error( 'JWT token invalid.' ) : $payload;
 	}
 
 	/**
@@ -27,11 +108,15 @@ class JWT {
 	 * @param string $jwt_access_token JWT access token.
 	 * @param string $jwt_id_token JWT id token.
 	 * @param string $refresh_token Opaque refresh token.
-	 * @return array
+	 * @return false|array Return FALSE if a refresh token could not be retrieved.
 	 */
-	public static function get_refresh_token( $jwt_access_token, $jwt_id_token, $refresh_token ) {
-		$id_token     = self::get_jwt_payload( $jwt_id_token );
-		$access_token = self::get_jwt_payload( $jwt_access_token );
+	public function get_refresh_token( $jwt_access_token, $jwt_id_token, $refresh_token ) {
+		$id_token     = $this->get_payload( $jwt_id_token );
+		$access_token = $this->get_payload( $jwt_access_token );
+
+		if ( is_wp_error( $id_token ) || is_wp_error( $access_token ) ) {
+			return array();
+		}
 
 		return array(
 			'client_id'     => $access_token['client_id'],
