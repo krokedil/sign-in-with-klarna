81e1d6b5ef89c7040aa0671b8b678ea72d79b8d2
anyakro <anya@krokedil.se>
2025-05-22 15:51:33 +0200

    Run settings update on init

--- src/Settings.php
+++ src/Settings.php
@@ -88,16 +88,25 @@ class Settings {
 	private $scope;
 
 	/**
+	 * The SIWK settings.
+	 *
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
 	 * Class constructor
 	 *
 	 * @param array $settings The settings to extract from.
 	 */
 	public function __construct( $settings ) {
-		$settings = wp_parse_args(
+		$settings       = wp_parse_args(
 			$settings,
 			$this->default(),
 		);
-		$this->update( $settings );
+		$this->settings = $settings;
+
+		add_action( 'init', array( $this, 'init_settings' ), 10, 0 );
 
 		if ( isset( $settings['siwk_callback_url'] ) && empty( $settings['siwk_callback_url'] ) ) {
 			$settings['siwk_callback_url'] = Redirect::get_callback_url();
@@ -105,6 +114,15 @@ class Settings {
 		}
 
 		add_filter( 'wc_gateway_klarna_payments_settings', array( $this, 'extend_settings' ) );
+	}
+
+	/**
+	 * Initialize the settings.
+	 *
+	 * @return void
+	 */
+	public function init_settings() {
+		$this->update( $this->settings );
 	}
 
 	/**
